396b49d63ef5fd647e8f0166563be6765c1f87f8
Faiz Rahmadani <fzrahmadan@gmail.com>
2024-10-27 16:33:43 +0700

    Add : - Update Controller and Update Stock Reqest Dto

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -45,5 +45,27 @@ namespace api.Controllers
   }
 
 
+  [HttpPut]
+  [Route("{id}")]
+  public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+  {
+   //searching algorithm
+   var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+   if (stockModel == null)
+   {
+    return NotFound();
+   }
+   stockModel.Symbol = updateDto.Symbol;
+   stockModel.CompanyName = updateDto.CompanyName;
+   stockModel.Purchase = updateDto.Purchase;
+   stockModel.LastDiv = updateDto.LastDiv;
+   stockModel.Industry = updateDto.Industry;
+   stockModel.MarketCap = updateDto.MarketCap;
+   _context.SaveChanges();
+   return Ok(stockModel.ToStockDto());
+
+  }
+
+
  }
 }
