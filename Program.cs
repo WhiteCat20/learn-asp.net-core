edeaf0bfd5ce6497f5192d13e1e5b2febc34482e
Faiz Rahmadani <fzrahmadan@gmail.com>
2024-10-27 13:06:26 +0700

    add : - menambahkan controller untuk mengambil semua data, dan berdasarkan ID. - menyambungkan dengan MS SQL

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// add controller here
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -19,11 +22,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // app.UseSwagger();
+    // app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
+// Add here to
+app.MapControllers();
 
 app.Run();
