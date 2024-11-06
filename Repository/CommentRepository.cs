61c3ed0364f0a5f6cb0e3a87e5e9cbd097903921
Faiz Rahmadani <fzrahmadan@gmail.com>
2024-11-06 20:38:26 +0700

    feat : - adding DELETE method for comments

--- Repository/CommentRepository.cs
+++ Repository/CommentRepository.cs
@@ -20,6 +20,8 @@ namespace api.Repository
             return comment;
         }
 
+
+
         public async Task<List<Comment>> GetAllAsync()
         {
             return await _context.Comments.ToListAsync();
@@ -43,6 +45,19 @@ namespace api.Repository
             await _context.SaveChangesAsync();
 
             return existingComment;
+        }
+
+        public async Task<Comment?> DeleteAsync(int id)
+        {
+            var commentModel = await _context.Comments.FirstOrDefaultAsync(x => x.Id == id);
+            if (commentModel == null)
+            {
+                return null;
+            }
+            _context.Comments.Remove(commentModel);
+            await _context.SaveChangesAsync();
+            return commentModel;
+
         }
     }
 }
